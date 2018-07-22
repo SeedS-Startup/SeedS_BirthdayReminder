08147e8f575dee0b679872dd7aa355c3bfa84ee0
Mohammad Jalili Torkamani <mohammad.jalili.torkamani@gmail.com>
2018-07-22 11:20:38 +0430

    Initial Commit

---
+++ app/src/main/java/com/seeds/seeds_birthdayreminder/Management/MainActivity.java
@@ -0,0 +1,15 @@
+package com.seeds.seeds_birthdayreminder.Management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seeds.seeds_birthdayreminder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
