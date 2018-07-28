4e2009941b9341fb7b47eefed05a6a778498974b
NargesAk <narges.ak12@gmail.com>
2018-07-28 23:37:16 +0430

    some chenges in convertor & BirthdayListAdapter

--- Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/BirthdayListAdapter.java
+++ Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/BirthdayListAdapter.java
@@ -6,11 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.seeds.seeds_birthdayreminder.Entity.BirthdayEvent;
 import com.seeds.seeds_birthdayreminder.R;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class BirthdayListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<BirthdayEvent> birthdayEvents;
@@ -38,7 +42,22 @@ public class BirthdayListAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         BirthdayEvent birthdayEvent=birthdayEvents.get(position);
-        //holder.itemView.findViewById(R.id.fullName).setT
+        TextView age=holder.itemView.findViewById(R.id.fullName);
+        TextView fullName=holder.itemView.findViewById(R.id.fullName);
+        TextView day=holder.itemView.findViewById(R.id.day);
+        TextView month=holder.itemView.findViewById(R.id.month);
+        TextView year=holder.itemView.findViewById(R.id.year);
+        TextView daysToEvent=holder.itemView.findViewById(R.id.days_to_event);
+        ImageView picture=holder.itemView.findViewById(R.id.picture);
+
+        age.setText(birthdayEvent.getAge());
+        fullName.setText(birthdayEvent.getFullName());
+        day.setText(birthdayEvent.getBirthDate().get(Calendar.DAY_OF_MONTH));
+        month.setText(birthdayEvent.getBirthDate().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH ));
+        year.setText(birthdayEvent.getBirthDate().get(Calendar.YEAR));
+        //daysToEvent.setText(birthdayEvent.);
+       // picture.set
+
 
     }
 
--- Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Configuration/Convertor.java
+++ Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Configuration/Convertor.java
@@ -1,10 +1,94 @@
 package com.seeds.seeds_birthdayreminder.Configuration;
 
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Convertor {
+    public static Calendar MYSQLI_StringToCalendar(String date) {
+        if (date == null || date.length() == 0)
+            return Calendar.getInstance();
+        try {
+            if (date.indexOf(".") > -1) {
+                date = date.substring(0, date.indexOf("."));
+            }
+            StringTokenizer tokenizer = new StringTokenizer(date, " ");
 
-    public static int getDaysBetween(Calendar calendar1, Calendar calendar2) {
-        return -1;
+            String datePart = tokenizer.nextToken();
+            String timePart = tokenizer.nextToken();
+
+            tokenizer = new StringTokenizer(datePart, "-");
+            String year = tokenizer.nextToken();
+            String month = tokenizer.nextToken();
+            String day = tokenizer.nextToken();
+
+            tokenizer = new StringTokenizer(timePart, ":");
+            String hours = tokenizer.nextToken();
+            String minutes = tokenizer.nextToken();
+            String seconds = tokenizer.nextToken();
+
+            Calendar finalDate = Calendar.getInstance();
+            finalDate.set(Calendar.YEAR, Integer.parseInt(year));
+            finalDate.set(Calendar.MONTH, Integer.parseInt(month) - 1);
+            finalDate.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day));
+
+            finalDate.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hours));
+            finalDate.set(Calendar.MINUTE, Integer.parseInt(minutes));
+            finalDate.set(Calendar.SECOND, Integer.parseInt(seconds));
+            return finalDate;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    public static String toCamelCase(String text) {
+        if (text == null)
+            return "Null";
+
+        final StringBuilder ret = new StringBuilder(text.length());
+
+        for (final String word : text.split(" ")) {
+            if (!word.isEmpty()) {
+                ret.append(word.substring(0, 1).toUpperCase());
+                ret.append(word.substring(1).toLowerCase());
+            }
+            if (!(ret.length() == text.length()))
+                ret.append(" ");
+        }
+
+        return ret.toString();
+    }
+
+    public static String getDaysBetween(Calendar calendarPrecede, Calendar calendarBefore) {
+
+        long difference = calendarPrecede.getTimeInMillis() - calendarBefore.getTimeInMillis();
+        int hours = (int) (difference / (1000 * 60 * 60));
+        int days = (int) (difference / (1000 * 60 * 60 * 24));
+        int months = (int) (difference / (1000 * 60 * 60 * 24 * 30));
+        int years = (int) (difference / (1000 * 60 * 60 * 24 * 30 * 12));
+
+        if (years != 0) {
+
+            return Math.abs(years) + " years "+(years<0 ? "ago " : "");
+
+        }
+        else if (months != 0)
+            return Math.abs(months) + " months "+(months<0 ? "ago" : "");
+        else if (days != 0)
+            return Math.abs(days) + " days "+(days<0 ? "ago" : "");
+        else
+            return Math.abs(hours) + " hours "+(hours<0 ? "ago" : "");
+
+    }
+
+    public static HashSet<String> putDoubleQuotationAroundEachItemOfHashSet(HashSet<String> set) {
+        HashSet<String> hashSet=new HashSet<>();
+        for(String item:set)
+        {
+            hashSet.add("\""+item+"\"");
+        }
+        return hashSet;
+    }
+
 }
