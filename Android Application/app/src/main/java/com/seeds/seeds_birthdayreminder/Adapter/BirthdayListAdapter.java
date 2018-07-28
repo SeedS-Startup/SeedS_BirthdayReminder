ca072a5458bc875aa64f2fcc997a76a1753f773b
NargesAk <narges.ak12@gmail.com>
2018-07-28 18:30:51 +0430

    adapters complete

--- Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/BirthdayListAdapter.java
+++ Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/BirthdayListAdapter.java
@@ -3,30 +3,47 @@ package com.seeds.seeds_birthdayreminder.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.seeds.seeds_birthdayreminder.Entity.BirthdayEvent;
+import com.seeds.seeds_birthdayreminder.R;
 
 import java.util.List;
 
 public class BirthdayListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    public BirthdayListAdapter(List<BirthdayEvent> items, Context context, RecyclerView birthdayRecyclerView) {
+    private List<BirthdayEvent> birthdayEvents;
+    private Context context;
+    private RecyclerView birthdayRecyclerView;
 
+    public BirthdayListAdapter(List<BirthdayEvent> items, Context context, RecyclerView birthdayRecyclerView) {
+        birthdayEvents=items;
+        this.context=context;
+        this.birthdayRecyclerView=birthdayRecyclerView;
+
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View view) {
+            super(view);
+        }
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.birthday_item_xml,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        BirthdayEvent birthdayEvent=birthdayEvents.get(position);
+       // holder.itemView.findViewById(R.id.).
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return birthdayEvents.size();
     }
 }
--- Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/CalendarListAdapter.java
+++ Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Adapter/CalendarListAdapter.java
@@ -3,29 +3,44 @@ package com.seeds.seeds_birthdayreminder.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.seeds.seeds_birthdayreminder.Entity.CalendarItem;
+import com.seeds.seeds_birthdayreminder.R;
 
 import java.util.List;
 
 public class CalendarListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<CalendarItem> calendarItems;
+    private Context context;
+    private  RecyclerView calendarRecyclerView;
     public CalendarListAdapter(List<CalendarItem> items, Context context, RecyclerView calendarRecyclerView) {
+        calendarItems=items;
+        this.context=context;
+        this.calendarRecyclerView=calendarRecyclerView;
     }
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View view) {
+            super(view);
+        }
+    }
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.calendar_item_xml,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        CalendarItem calendarItem=calendarItems.get(position);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return calendarItems.size();
     }
 }
