428118b217f87a4c0957ee60e84bbea57cebb242
Mohammad Jalili Torkamani <mohammad.jalili.torkamani@gmail.com>
2018-07-29 20:25:36 +0430

    day item - calendar item - added

--- Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Entity/CalendarItem.java
+++ Android Application/app/src/main/java/com/seeds/seeds_birthdayreminder/Entity/CalendarItem.java
@@ -1,4 +1,43 @@
 package com.seeds.seeds_birthdayreminder.Entity;
 
+import android.view.TextureView;
+import android.view.View;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class CalendarItem {
+    private int year;
+    private int month;
+    private Set<DayItem> days;  //size : 31 or 30
+
+    public CalendarItem(int year, int month, Set<DayItem> days) {
+        this.year = year;
+        this.month = month;
+        this.days = days;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public Set<DayItem> getDays() {
+        return days;
+    }
+
+    public void setDays(Set<DayItem> days) {
+        this.days = days;
+    }
 }
